8184401f33f74300ee9ac10c29d04f6a2d7b0d42
Aja Singer <ajasinger@gmail.com>
2024-09-16 18:23:17 -0400

    add pagination

--- src/components/TableDisplay.jsx
+++ src/components/TableDisplay.jsx
@@ -6,6 +6,12 @@ export default function TableDisplay() {
     const [todos, setTodos] = useState(null);
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(false);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [pages, setPages] = useState([]);
+    const [todoPage, setTodoPage] = useState([]);
+
+    let totalPages = ''
+    const rowsPerPg = 10;
 
     //function
     useEffect(() => {
@@ -17,8 +23,26 @@ export default function TableDisplay() {
         try {
 
             const res = await fetch('https://jsonplaceholder.typicode.com/todos')
-            const resData = await res.json()
-            setTodos(resData);
+
+            if(!res.ok) {
+                console.log('error fetching data')
+                setError(true);
+            } else {
+                const resData = await res.json()
+                setTodos(resData);
+                const firstPage = resData.slice(0, rowsPerPg);
+                setTodoPage(firstPage);
+    
+                totalPages = resData.length / rowsPerPg;
+
+                //create array of pages 
+                let pageCount = []
+                for(let i = 1; i <= totalPages; i++) {
+                    pageCount.push(i);
+                }
+                setPages(pageCount);
+                console.log('pageCount', pageCount);
+            }
 
         } catch(error) {
             console.log(error);
@@ -46,21 +70,33 @@ export default function TableDisplay() {
         setTodos(sortTodos);
     }
 
+    const handlePageClick = (e) => {
+        setCurrentPage(e.target.value);
+        const sliceStart = rowsPerPg * e.target.value-1;
+        const sliceEnd = sliceStart + 10
+        const slicedTodos = todos.slice(sliceStart, sliceEnd);
+        setTodoPage(slicedTodos);
+    }
+
     return(
         <div className={styles.tableContainer}>
         <h1>Todos</h1>
         <button type="button" onClick={handleSort}>SORT</button>
         {/* map todos */}
         <div className="tableContainerArea">
-        {todos &&
-            todos?.map(todo => (
-                <div className="todo-item" key={todo.id}>
-                    {todo.title}
-                    <button className="todo-button" type="button" onClick={e => handleDelete(todo.id)}>x</button>
-                </div>
-            ))
-        }
+            {todoPage &&
+                todoPage?.map(todo => (
+                    <div className="todo-item" key={todo.id}>
+                        {todo.title}
+                        <button className="todo-button" type="button" onClick={e => handleDelete(todo.id)}>x</button>
+                    </div>
+                ))
+            }
         </div>
+        {/* pagination */}
+        {pages && pages.map((page, i) => (
+            <button value={page} key={i} onClick={handlePageClick} className="page-num">{page}</button>
+        ))}
         {loading &&
             <p>loading..</p>
         }
