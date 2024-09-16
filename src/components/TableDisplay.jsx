e2b94cbde7155bbe768c02cb061e234f4aad918b
Aja Singer <ajasinger@gmail.com>
2024-09-16 14:54:54 -0400

    fetch data and dsplay it

--- src/components/TableDisplay.jsx
+++ src/components/TableDisplay.jsx
@@ -1,14 +1,71 @@
-import { useEffect } from "react";
+import { useState, useEffect, Fragment } from "react";
 
 export default function TableDisplay() {
-    //useEffect with GET request 
+    //state
+    const [todos, setTodos] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(false);
 
-    //mutate data 
+    //function
+    useEffect(() => {
+        fetchTodos();
+    }, []);
 
-    //map data to table with pagination 
+    const fetchTodos = async() => {
+        setLoading(true);
+        try {
+
+            const res = await fetch('https://jsonplaceholder.typicode.com/todos')
+            const resData = await res.json()
+
+            console.log('resData', resData);
+            setTodos(resData);
+
+        } catch(error) {
+            console.log(error);
+            setError(true);
+        } finally {
+            setLoading(false);
+        }
+    }
+
+    const handleDelete = (id) => {
+        const deleteTodo = todos.filter(todo => id !== todo.id);
+        setTodos(deleteTodo);
+    }
+
+    const handleSort = () => {
+        const sortTodos = [...todos].sort((a,b) => {
+            if(a.title < b.title) {
+                return -1
+            } 
+            if(a.title > b.title) {
+                return 1
+            }
+            return 0;
+        })
+        setTodos(sortTodos);
+    }
 
     return(
         <>
+        <h1>Todos</h1>
+        <button type="button" onClick={handleSort}>SORT</button>
+        {/* map todos */}
+        {todos &&
+            todos?.map(todo => (
+                <div key={todo.id}>
+                    {todo.title}
+                    <button type="button" onClick={e => handleDelete(todo.id)}>x</button>
+                </div>
+            ))
+        }
+        {loading &&
+            <p>loading..</p>
+        }
+        {error &&
+            <p>There was an error loading todos.</p>
+        }
         </>
     )
 }
