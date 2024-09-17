8e118f60af3919562e471b232f756345a7c6b1fa
Aja Singer <ajasinger@gmail.com>
2024-09-17 14:27:08 -0400

    code scroll

--- src/components/ImageCarousel.jsx
+++ src/components/ImageCarousel.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import styles from './ImageCarousel.module.css';
 
 export default function ImageCarousel() {
@@ -8,6 +8,7 @@ export default function ImageCarousel() {
     const [error, setError] = useState(false);
     const [loading, setLoading] = useState(false);
     const [carouselPosition, setCarouselPosition] = useState(null);
+    const ref = useRef(null);
 
     //FUNCTIONS 
     //onClick for arrow to set -- setPosition() -- maybe in UI
@@ -50,6 +51,19 @@ export default function ImageCarousel() {
         postData();
     }, [])
 
+    const handleScroll = (direction) => {
+        const container = ref.current;
+        const scrollAmount = 300;
+
+        if(direction === 'left') {
+            container.scrollBy({ left: -scrollAmount, behavior: 'smooth'})
+        }
+
+        if(direction === 'righ') {
+            container.scrollBy({ left: scrollAmount, behavior: 'smooth'})
+        }
+    }
+
     return(
         <div className={styles.outerContainer}>
             <h1>Carousel</h1>
@@ -57,7 +71,7 @@ export default function ImageCarousel() {
                     <button className={styles.arrow}>&#8592;</button>
                     <button className={styles.arrow}>&#8594;</button>
                 </div>
-            <div className={styles.container}>
+            <div className={styles.container} ref={ref}>
                 {imagesArray && imagesArray.length > 0 &&
                     imagesArray.map(image => (
                         <div key={image?.id} className={carouselPosition === image?.id ? styles.focus : styles.notFocus}>
