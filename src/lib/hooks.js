ca6b67580df5a665bf3d29f1bf27972fd3a9a6f2
Aja Singer <ajasinger@gmail.com>
2024-09-17 16:37:28 -0400

    add custom hooks

---
+++ src/lib/hooks.js
@@ -0,0 +1,14 @@
+import { useState, useEffect } from "react";
+
+export function useLocalStorage() {
+    //string is desired key to save in localStorage
+    const [value, setValue] = useState((str) => {
+        //get data from localStorage
+        return JSON.parse(localStorage.getItem(str))
+    });
+
+    useEffect(() => {
+        //set data in localStorage
+        localStorage.setValue(str, JSON.stringify(value))
+    }, [value])
+}
