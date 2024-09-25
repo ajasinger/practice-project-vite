3c1bb024f62873b3b0a2a3472ae8195473e9b491
Aja Singer <ajasinger@gmail.com>
2024-09-25 13:08:58 -0400

    define methods component

---
+++ src/components/Methods.jsx
@@ -0,0 +1,20 @@
+// We provided a small React application with some starter code. Your goal is to modify the application so that it displays a 
+// tile for every letter in the alphabet in uppercase format. Then if a tile is clicked, that letter is appended to the current 
+// string that exists in the element with ID outputString.
+
+// If at any point there are 3 consecutive letters that are the same, replace them with an underscore. 
+// For example, if A, B, C, F, F, F, G is clicked in that order, the string that appears in outputString would be ABC_G. 
+// If 6 of the same letter appears after, for example, clicking A six times followed by a B, then outputString would be __B.
+
+// You are free to add classes and styles, but make sure you leave the component ID's and classes provided as they are. 
+// Submit your code once it is complete and our system will validate your output.
+
+import {useState} from 'react';
+
+export default function Methods() {
+    return(
+        <div>
+
+        </div>
+    )
+}
