93eb3c9e48b8a952415de45448735556969cf81b
Aja Singer <ajasinger@gmail.com>
2024-09-30 16:54:08 -0400

    leetcode

--- src/components/Leetcode.jsx
+++ src/components/Leetcode.jsx
@@ -11,8 +11,74 @@ var removeDuplicates = function(nums) {
 };
 
 //leetcode 27 (remove element)
-//remove all val in nums, modify array 
-//return number of unique elements 
 var removeElement = function(nums, val) {
-    
+    let k = 0
+    for(let i = 0; i < nums.length; i++) {
+        if(nums[i] !== val) {
+            nums[k] = nums[i];
+            k++
+        }
+    }
+    return k
+};
+
+//1929 concatenate array 
+var getConcatenation = function(nums) {
+    return ans = [...nums,...nums]
+    //return nums.concat(nums)
+};
+
+//20 valid parentheses
+var isValid = function(s) {
+    let stack = [];
+    const closeToOpen = {
+        ")" : "(",
+        "}" : "{",
+        "]" : "["
+    }
+
+    for(let char of s) {
+        if(char in closeToOpen) {
+            //if key (closing bracket) check stack for a match
+            if (stack.length === 0 || stack[stack.length - 1] !== closeToOpen[char]) {
+                return false; // No match found or stack is empty
+            }
+            stack.pop(); // Remove the matching opening bracket from the stack
+        } else {
+            //if open bracket
+            stack.push(char);
+        }
+    }
+
+    return stack.length === 0;
+}
+
+
+var isValid2 = function(s) {
+    // Stack to store opening brackets
+    let stack = [];
+
+    // HashMap to store valid pairs of opening and closing brackets
+    let bracketMap = {
+        ')': '(',
+        '}': '{',
+        ']': '['
+    };
+
+    // Traverse the string
+    for (let char of s) {
+        if (char === '(' || char === '{' || char === '[') {
+            // Push opening brackets onto the stack
+            stack.push(char);
+        } else {
+            // If the character is a closing bracket, check for a match
+            if (stack.length === 0 || stack[stack.length - 1] !== bracketMap[char]) {
+                return false; // No match found or stack is empty
+            }
+            stack.pop(); // Remove the matching opening bracket from the stack
+        }
+    }
+
+    // If the stack is empty, all opening brackets had matching closing brackets
+    return stack.length === 0;
 };
