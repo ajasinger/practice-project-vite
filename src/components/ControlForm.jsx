b07d24f212b0e127329d958dd07914687bcd3f5e
Aja Singer <ajasinger@gmail.com>
2024-10-16 16:10:39 -0400

    add error handling and styling

--- src/components/ControlForm.jsx
+++ src/components/ControlForm.jsx
@@ -1,3 +1,4 @@
+import './controlForm.css'
 import { useState } from "react"
 
 //re-renders on every change to input as user is typing 
@@ -9,6 +10,7 @@ export default function ControlForm() {
         password: '', 
         confirmPassword: ''
     });
+    const [focus, setFocus] = useState(false);
 
     const inputs = [
         {
@@ -16,28 +18,39 @@ export default function ControlForm() {
             name: "username",
             type: "text",
             placeholder: "username",
-            label: "username"
+            label: "username",
+            errorMessage: "username should be 3-16 characters with no special characters",
+            pattern: "^[a-zA-Z0-9$_@-]{3,16}$",
+            required: true,
         },
         {
             id: 2,
             name: "email",
             type: "email",
             placeholder: "email",
-            label: "email"
+            label: "email",
+            errorMessage: "this must ba a valid email address",
+            required: true
         },
         {
             id: 3,
             name: "password",
             type: "password",
             placeholder: "password",
-            label: "password"
+            label: "password",
+            errorMessage: "password should be 8-20 characters and should include at least 1 letter, 1 number, and 1 special character",
+            pattern: "^[a-zA-Z0-9$_@-]{8,20}$",
+            required: true
         },
         {
             id: 4,
             name: "confirmPassword",
             type: "password",
             placeholder: "password",
-            label: "confirmPassword"
+            label: "confirmPassword",
+            errorMessage: "passwords don't match",
+            pattern: values.password,
+            required: true
         },
 
     ]
@@ -59,7 +72,12 @@ export default function ControlForm() {
                             value={values[input.name]}
                             placeholder={input.placeholder}
                             onChange={e => setValues({...values, [e.target.name]:e.target.value})}
+                            required={input.required}
+                            pattern-={input.pattern}
+                            onBlur={e => setFocus(true)}
+                            focused={focus.toString()}
                         />
+                        <div>{errorMessage}</div>
                     </div>
                 ))}
                 <button>Submit</button>
