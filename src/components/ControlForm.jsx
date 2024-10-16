bc1e1ac8b821091918926a174729392719e1a1f3
Aja Singer <ajasinger@gmail.com>
2024-10-16 14:25:53 -0400

    map inputs

--- src/components/ControlForm.jsx
+++ src/components/ControlForm.jsx
@@ -1,24 +1,68 @@
 import { useState } from "react"
 
-//re-remders on every change to input as user is typing 
+//re-renders on every change to input as user is typing 
 
 export default function ControlForm() {
-    const [username, setUsername] = useState('');
-    const [email, setEmail] = useState('')
+    const [values, setValues] = useState({
+        username: '',
+        email: '',
+        password: '', 
+        confirmPassword: ''
+    });
+
+    const inputs = [
+        {
+            id: 1,
+            name: "username",
+            type: "text",
+            placeholder: "username",
+            label: "username"
+        },
+        {
+            id: 2,
+            name: "email",
+            type: "email",
+            placeholder: "email",
+            label: "email"
+        },
+        {
+            id: 3,
+            name: "password",
+            type: "password",
+            placeholder: "password",
+            label: "password"
+        },
+        {
+            id: 4,
+            name: "confirmPassword",
+            type: "password",
+            placeholder: "password",
+            label: "confirmPassword"
+        },
+
+    ]
+
+    const handleSubmit = e => {
+        e.preventDefault();
+
+    }
 
     return(
         <div>
-            <form>
-                <label>Username</label>
-                <input 
-                    placeholder="username"
-                    onChange={e => setUsername(e.target.value)}
-                />
-                <label>Email</label>
-                <input 
-                    placeholder="email"
-                    onChange={e => setEmail(e.target.value)}
-                />
+            <form onSubmit={handleSubmit}>
+                {inputs.map (input => (
+                    <div key={input.id}>
+                        <label>{input.label}</label>
+                        <input 
+                            name={input.name}
+                            type={input.type}
+                            value={values[input.name]}
+                            placeholder={input.placeholder}
+                            onChange={e => setValues({...values, [e.target.name]:e.target.value})}
+                        />
+                    </div>
+                ))}
+                <button>Submit</button>
             </form>
             
         </div>
