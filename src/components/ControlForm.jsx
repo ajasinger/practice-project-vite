4050e85695c378a768ccdd28afc1e092d9106d54
Aja Singer <ajasinger@gmail.com>
2024-10-21 15:35:58 -0400

    silvershield input example

--- src/components/ControlForm.jsx
+++ src/components/ControlForm.jsx
@@ -204,3 +204,67 @@ export default function ControlForm() {
 //         </div>
 //     );
 // }
+
+
+//SILVERSHIELD PASSWORD INPUT
+
+// "use client";
+
+// import { useState } from 'react';
+// import { usePathname } from 'next/navigation';
+
+// export default function PasswordInput({password, setPassword}) {
+//     const [visible, setVisible] = useState(false);
+//     const pathname = usePathname();
+
+//     const fontStyle = (pathname === '/update-password') ? "text-xs font-bold" : "text-base";
+//     const inputStyle = (pathname === '/update-password') ?
+//         "text-gray-900 text-sm border-silverBlue-2 border block w-full px-2.5 py-2 focus:outline-none focus:ring-0 invalid:[&:not(:placeholder-shown):not(:focus)]:border-red-500 peer" :
+//         "text-gray-900 text-sm border-silverBlue-dark border block w-full p-3 focus:outline-none focus:ring-0 invalid:[&:not(:placeholder-shown):not(:focus)]:border-red-500 peer"
+
+//     return(
+//         <div>
+//                 <div className="space-y-2">
+//                     <label 
+//                         htmlFor="password"
+//                         className={fontStyle}
+//                     >
+//                         Password
+//                     </label>
+//                     <div className="flex w-full flex-col relative">
+//                         <input
+//                             type={visible ? "text" : "password"}
+//                             name="password"
+//                             id="password"
+//                             value={password}
+//                             autoComplete="new-password"
+//                             className={inputStyle}
+//                             placeholder="password"
+//                             onChange = {(e) => setPassword(e.target.value)}
+//                             required
+//                             pattern = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$"                     
+//                         />
+//                         <span 
+//                             onClick={() => setVisible(!visible)}
+//                             className="flex justify-around items-center"
+//                         >
+//                             {visible ? (
+//                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-5 h-5 absolute right-2 top-3">
+//                                     <path strokeLinecap="round" strokeLinejoin="round" d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z" />
+//                                     <path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+//                                 </svg>
+//                             ) : (
+//                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-5 h-5 absolute right-2 top-3">
+//                                     <path strokeLinecap="round" strokeLinejoin="round" d="M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88" />
+//                                 </svg>
+//                             )}
+//                         </span>
+//                         <div className="mt-2 hidden text-sm text-red-500 peer-[&:not(:placeholder-shown):not(:focus):invalid]:block">
+//                             Password should include at least 8 characters, 1 lower case letter, 1 upper case letter, 1 number, and 1 special character from @$!%*?&
+//                         </div>
+//                     </div>
+//                 </div>
+
+//         </div>
+//     )
+// }
