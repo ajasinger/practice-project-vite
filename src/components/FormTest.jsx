183a2dcd78de3c77dc52de45376d556ee4f38d9c
Aja Singer <ajasinger@gmail.com>
2024-10-25 14:11:53 -0400

    FormData code

--- src/components/FormTest.jsx
+++ src/components/FormTest.jsx
@@ -3,18 +3,24 @@ export default function TestForm() {
 
     //UNCONTROLLED FORM
 
-
     //FORMDATA
+
+    const handleSubmit = e => {
+        e.preventDefault();
+
+        const formData = new FormData();
+        const email = formData.target.value
+    }
     
     return (
         <form onSubmit={handleSubmit}>
         <div>
             <label>Username:</label>
-            <input type="text" />
+            <input type="text" name="name"/>
         </div>
         <div>
             <label>Email:</label>
-            <input type="email" />
+            <input type="email" name="email"/>
         </div>
         <button type="submit">Submit</button>
         </form>
